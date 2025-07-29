8b57846a8a3b14682138350f1385e35f4ad19eaa
Ferespinal07 <fjer0001@ce.pucmm.edu.do>
2025-07-29 17:10:26 -0400

    Remove role checks from alert management

---
+++ src/main/java/org/javadominicano/cmp/controller/AlertRuleRestController.java
@@ -0,0 +1,40 @@
+package org.javadominicano.cmp.controller;
+
+import org.javadominicano.cmp.DatabaseManager;
+import org.javadominicano.cmp.dto.AlertRuleDTO;
+import org.javadominicano.cmp.dto.WeatherAlertDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class AlertRuleRestController {
+
+    private final DatabaseManager dbManager;
+
+    @Autowired
+    public AlertRuleRestController(DatabaseManager dbManager) {
+        this.dbManager = dbManager;
+    }
+
+    @GetMapping("/api/reglas-alerta")
+    public List<AlertRuleDTO> listarReglas() {
+        return dbManager.getAlertRuleDTOs();
+    }
+
+    @DeleteMapping("/api/reglas-alerta/{id}")
+    public ResponseEntity<Void> eliminarRegla(@PathVariable int id) {
+        dbManager.deleteAlertRule(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/api/alertas/ocurridas")
+    public List<WeatherAlertDTO> listarAlertasGeneradas() {
+        return dbManager.getWeatherAlerts();
+    }
+}
